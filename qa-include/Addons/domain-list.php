a28cc936f87b02afc4a99a72c989670bd713ab0d
selim song <491497047@qq.com>
2012-11-04 19:41:26 +0800

    remove the www domain issue

--- qa-include/Addons/domain-list.php
+++ qa-include/Addons/domain-list.php
@@ -2,4 +2,4 @@
 
 $domain_lists = array('japanquestion.com',  'koreaquestion.com', 'indiaquestion.com', 'brazilquestion.com',
                       'guangzhouquestion.com',
-                     'hongkongquestion.com', 'australiaquestion.com', 'egyptquestion.com', 'indonesiaquestion.com');
+                      'australiaquestion.com', 'egyptquestion.com', 'indonesiaquestion.com');
