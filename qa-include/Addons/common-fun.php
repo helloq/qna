07c60a694b996c7bd4e74ba3b04fea0c013284f1
selim song <491497047@qq.com>
2012-11-06 20:15:40 +0800

    refactor the get domainl list function

--- qa-include/Addons/common-fun.php
+++ qa-include/Addons/common-fun.php
@@ -5,5 +5,16 @@ function getCategoryByDomain($categoryslugs, $domain_lists){
 		$cityName = preg_replace('#www.|question.com#', '', $server_name);
 	    $categoryslugs = array($cityName);
     }
+	switch($server_name){
+		case "hongkongquestion.com":
+           $categoryslugs = array('hong-kong');
+		   break;
+		case  "new-zealandquestion.com":
+           $categoryslugs = array('new-zealand');
+		   break;
+	    case  "south-africaquestion.com":
+		   $categoryslugs = array('south-africa');
+		   break;
+	}
 	return $categoryslugs;
 }
